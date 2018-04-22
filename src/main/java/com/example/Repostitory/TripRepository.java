9a72d6a9e9178697606984fe1a69b9c752a2c6b3
Sameh16 <sameh_mohamed16@hotmail.com>
2018-04-22 20:27:22 +0200

    test

--- src/main/java/com/example/Repostitory/TripRepository.java
+++ src/main/java/com/example/Repostitory/TripRepository.java
@@ -1,6 +1,7 @@
 package com.example.Repostitory;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.springframework.data.repository.CrudRepository;
 
@@ -12,5 +13,6 @@ public interface TripRepository extends CrudRepository<Trip, Long> {
 
 	public ArrayList<Trip> findByDriver(Driver driver);
 	public Trip findFirstByTruckOrderByIdDesc(Truck truck);
+	public ArrayList<Trip> findByDateGreaterThanEqual(Date date);
 
 }
