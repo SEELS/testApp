0cc9448c1fa4ddaa1cbd8d46a4fb420b8400f4e4
Sameh16 <sameh_mohamed16@hotmail.com>
2018-04-30 00:52:18 +0200

    tripEdit

--- src/main/java/com/example/Repostitory/TripRepository.java
+++ src/main/java/com/example/Repostitory/TripRepository.java
@@ -12,7 +12,7 @@ import com.example.models.Truck;
 public interface TripRepository extends CrudRepository<Trip, Long> {
 
 	public ArrayList<Trip> findByDriver(Driver driver);
-	public Trip findFirstByTruckOrderByIdDescAndDeleted(Truck truck,boolean deleted);
+	public Trip findFirstByTruckOrderByIdDesc(Truck truck);
 	public ArrayList<Trip> findByDriverAndDeletedAndDateGreaterThanEqual(Driver driver,boolean deleted,Date date);
 
 }
